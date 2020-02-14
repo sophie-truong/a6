802951337d2d65775b5e551da975ca9ff6600d7c
Sophie <sbt001@ucsd.edu>
2020-02-13 18:26:40 -0800

    sobs

--- routes/shared.js
+++ routes/shared.js
@@ -1,7 +1,10 @@
 exports.viewProject = function(req, res){
   var name = req.params.name;
+  var them = req.params.them;
+
   console.log("This friend is:" + name);
   res.render('shared', {
-    "friendName": name
+    "friendName": name,
+    "friendThem": them
   });
 }
