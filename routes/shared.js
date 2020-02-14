bf631e10adff2e0d40146aa2378d9cc14763d299
Sophie <sbt001@ucsd.edu>
2020-02-13 18:40:56 -0800

    im dying

--- routes/shared.js
+++ routes/shared.js
@@ -1,10 +1,12 @@
 exports.viewProject = function(req, res){
   var name = req.params.name;
   var them = req.params.them;
+  var you = req.params.you;
 
   console.log("This friend is:" + name);
   res.render('shared', {
     "friendName": name,
-    "friendThem": them
+    "friendThem": them,
+    "friendYou": you
   });
 }
